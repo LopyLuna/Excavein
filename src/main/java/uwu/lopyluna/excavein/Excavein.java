83d6b36926f888d8586df9e364c3e29470725282
Mayaqq <i@mayaqq.dev>
2024-11-10 16:34:40 +0100

    made config screen work and fixed issue with dedicated server

--- src/main/java/uwu/lopyluna/excavein/Excavein.java
+++ src/main/java/uwu/lopyluna/excavein/Excavein.java
@@ -2,6 +2,7 @@ package uwu.lopyluna.excavein;
 
 import com.mojang.logging.LogUtils;
 import fuzs.forgeconfigapiport.fabric.api.neoforge.v4.NeoForgeConfigRegistry;
+import fuzs.forgeconfigapiport.fabric.api.neoforge.v4.client.ConfigScreenFactoryRegistry;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
@@ -10,6 +11,7 @@ import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import org.slf4j.Logger;
 import uwu.lopyluna.excavein.config.ClientConfig;
 import uwu.lopyluna.excavein.config.ServerConfig;
@@ -24,7 +26,22 @@ public class Excavein implements ModInitializer {
     public static final String VERSION = "1.0a.Release";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    private void registerPackets() {
+    @Override
+    public void onInitialize() {
+        NeoForgeConfigRegistry.INSTANCE.register(MOD_ID, ModConfig.Type.CLIENT, ClientConfig.CLIENT_SPEC);
+        NeoForgeConfigRegistry.INSTANCE.register(MOD_ID, ModConfig.Type.SERVER, ServerConfig.SERVER_SPEC);
+
+        registerServerPackets();
+
+        ServerTickEvents.END_WORLD_TICK.register(CooldownTracker::onWorldTick);
+        ServerPlayConnectionEvents.JOIN.register(CooldownTracker::onPlayerLogin);
+
+        ServerTickEvents.END_WORLD_TICK.register(BlockPositionTracker::onWorldTick);
+
+        PlayerBlockBreakEvents.BEFORE.register(BlockPositionTracker::onBlockBreak);
+    }
+
+    private void registerServerPackets() {
         PayloadTypeRegistry.playC2S().register(KeybindPacket.TYPE, KeybindPacket.CODEC);
         PayloadTypeRegistry.playC2S().register(SelectionInspectionPacket.TYPE, SelectionInspectionPacket.CODEC);
         PayloadTypeRegistry.playS2C().register(SelectionOutlinePacket.TYPE, SelectionOutlinePacket.CODEC);
@@ -33,23 +50,5 @@ public class Excavein implements ModInitializer {
 
         ServerPlayNetworking.registerGlobalReceiver(KeybindPacket.TYPE, KeybindPacket::handle);
         ServerPlayNetworking.registerGlobalReceiver(SelectionInspectionPacket.TYPE, SelectionInspectionPacket::handle);
-        ClientPlayNetworking.registerGlobalReceiver(SelectionOutlinePacket.TYPE, SelectionOutlinePacket::handle);
-        ClientPlayNetworking.registerGlobalReceiver(CooldownPacket.TYPE, CooldownPacket::handle);
-        ClientPlayNetworking.registerGlobalReceiver(IsBreakingPacket.TYPE, IsBreakingPacket::handle);
-    }
-
-    @Override
-    public void onInitialize() {
-        NeoForgeConfigRegistry.INSTANCE.register(MOD_ID, ModConfig.Type.CLIENT, ClientConfig.CLIENT_SPEC);
-        NeoForgeConfigRegistry.INSTANCE.register(MOD_ID, ModConfig.Type.SERVER, ServerConfig.SERVER_SPEC);
-
-        registerPackets();
-
-        ServerTickEvents.END_WORLD_TICK.register(CooldownTracker::onWorldTick);
-        ServerPlayConnectionEvents.JOIN.register(CooldownTracker::onPlayerLogin);
-
-        ServerTickEvents.END_WORLD_TICK.register(BlockPositionTracker::onWorldTick);
-
-        PlayerBlockBreakEvents.BEFORE.register(BlockPositionTracker::onBlockBreak);
     }
 }
--- src/main/java/uwu/lopyluna/excavein/ExcaveinClient.java
+++ src/main/java/uwu/lopyluna/excavein/ExcaveinClient.java
@@ -1,22 +1,38 @@
 package uwu.lopyluna.excavein;
 
+import fuzs.forgeconfigapiport.fabric.api.neoforge.v4.client.ConfigScreenFactoryRegistry;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import uwu.lopyluna.excavein.client.BlockOutlineRenderer;
 import uwu.lopyluna.excavein.client.KeybindHandler;
 import uwu.lopyluna.excavein.client.ModeOverlay;
+import uwu.lopyluna.excavein.network.CooldownPacket;
+import uwu.lopyluna.excavein.network.IsBreakingPacket;
+import uwu.lopyluna.excavein.network.SelectionOutlinePacket;
+
+import static uwu.lopyluna.excavein.Excavein.MOD_ID;
 
 public class ExcaveinClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        ConfigScreenFactoryRegistry.INSTANCE.register(MOD_ID, ConfigurationScreen::new);
+
         KeybindHandler.register();
         WorldRenderEvents.BLOCK_OUTLINE.register(BlockOutlineRenderer::onRenderWorld);
         ClientTickEvents.END_CLIENT_TICK.register(BlockOutlineRenderer::onClientTick);
 
         HudRenderCallback.EVENT.register(ModeOverlay::onRenderGuiOverlay);
         ClientTickEvents.END_CLIENT_TICK.register(ModeOverlay::onClientTick);
+    }
+
+    public void registerClientPackets() {
+        ClientPlayNetworking.registerGlobalReceiver(SelectionOutlinePacket.TYPE, SelectionOutlinePacket::handle);
+        ClientPlayNetworking.registerGlobalReceiver(CooldownPacket.TYPE, CooldownPacket::handle);
+        ClientPlayNetworking.registerGlobalReceiver(IsBreakingPacket.TYPE, IsBreakingPacket::handle);
     }
 }
